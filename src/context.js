cd44118e13bdf8c799c39a1c8942666912054e07
Riya5624 <105262781+Riya5624@users.noreply.github.com>
2023-01-05 21:20:48 +0530

    Initial commit

---
+++ src/context.js
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect, useState } from "react";
+
+const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+const AppContext = React.createContext();
+const AppProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const [isError, setIsError] = useState({ show: "false", msg: "" });
+  const [query, setQuery] = useState("marvel");
+  async function getMovies(url) {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      if (data.Response === "True") {
+        setIsLoading(false);
+        setMovie(data.Search);
+      } else {
+        setIsError({
+          show: true,
+          msg: data.error
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getMovies(`${API_URL}&s=${query}`);
+  }, [query]);
+
+  return (
+    <AppContext.Provider value={{ movie, isLoading, isError, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider, useGlobalContext, API_URL };
